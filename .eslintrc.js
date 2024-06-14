6dd9e045f262712c7c447dd1724f2db580c196bb
skytomo <tomo1kawa.fullerene@gmail.com>
2024-06-14 21:50:09 +0900

    refactor: format

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    project: "./tsconfig.json",
   },
   plugins: ["@typescript-eslint", "react"],
   rules: {
--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,45 @@
-import React from "react";
+import React from 'react';
 
-import data from "./data.json";
+import data from './data.json';
 
-export function App() {
+export default function App() {
   const { terrors, wikis } = data;
-  const [value, setValue] = React.useState("");
-  const [wiki, setWiki] = React.useState<keyof typeof wikis>("ton-jp.wiki");
+  const [searchWord, setSearchWord] = React.useState('');
+  const [selectedWiki, setSelectedWiki] = React.useState<keyof typeof wikis>('ton-jp.wiki');
 
-  return <div>
-    <select name="pets" id="pet-select"
-      onChange={(e) => { setWiki(e.target.value as keyof typeof wikis) }} >
-      {
-        Object.keys(wikis).map((wiki) => <option key={wiki} value={wiki}>{wiki}</option>)
-      }
-    </select>
-    <input
-      value={value}
-      onChange={(e) => setValue(e.target.value)}
-      placeholder="検索……"
-    />
-    {
-      terrors
-        .filter((terror) => terror.name.toLowerCase().startsWith(value.toLowerCase()))
-        .map((terror) => <a key={terror.name} href={terror.links && terror.links[wiki] || wikis[wiki].terrorsLink.replace("$terror", terror.name)}>{terror.name}</a>)
-    }</div>;
+  return (
+    <div>
+      <select
+        name="pets"
+        id="pet-select"
+        onChange={(e) => {
+          setSelectedWiki(e.target.value as keyof typeof wikis);
+        }}
+      >
+        {Object.keys(wikis).map((wiki) => (
+          <option key={wiki} value={wiki}>
+            {wiki}
+          </option>
+        ))}
+      </select>
+      <input
+        value={searchWord}
+        onChange={(e) => setSearchWord(e.target.value)}
+        placeholder="検索……"
+      />
+      {terrors
+        .filter((terror) => terror.name.toLowerCase().startsWith(searchWord.toLowerCase()))
+        .map((terror) => (
+          <a
+            key={terror.name}
+            href={
+              (terror.links && terror.links[selectedWiki])
+              || wikis[selectedWiki].terrorsLink.replace('$terror', terror.name)
+            }
+          >
+            {terror.name}
+          </a>
+        ))}
+    </div>
+  );
 }
