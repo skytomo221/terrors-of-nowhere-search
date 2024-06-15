616f22cce09e3adf977d1eaef5be9f0fd9de4edf
skytomo <tomo1kawa.fullerene@gmail.com>
2024-06-15 11:25:52 +0900

    fix: error

--- src/images.ts
+++ src/images.ts
@@ -4,6 +4,7 @@ const images = imagesContext
   .reduce<{ [key: string]: any }>((images, path) => {
     // ファイル名をキーとして画像を格納します
     const key = path.replace("./", "").replace(".png", "").replace(".gif", "");
+    // eslint-disable-next-line no-param-reassign
     images[key] = imagesContext(path);
     return images;
   }, {});
