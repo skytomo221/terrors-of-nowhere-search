0fc4511e2dde923a9bce43ea259c6b77142d053d
skytomo <tomo1kawa.fullerene@gmail.com>
2024-06-16 23:15:41 +0900

    fix: #8 gif

--- src/images.ts
+++ src/images.ts
@@ -1,9 +1,9 @@
-const imagesContext = require.context("./assets/images", false, /\.png$/);
+const imagesContext = require.context("./assets/images", false, /\.(png|gif)$/);
 const images = imagesContext
   .keys()
   .reduce<{ [key: string]: any }>((images, path) => {
     // ファイル名をキーとして画像を格納します
-    const key = path.replace("./", "").replace(".png", "").replace(".gif", "");
+    const key = path.replace("./", "").replace(/.(png|gif)$/, "")
     // eslint-disable-next-line no-param-reassign
     images[key] = imagesContext(path);
     return images;
